d606504d564e894fb029e9abb3cee406246a17b9
zvonkin <mzvx8@mst.edu>
2020-09-09 23:44:42 -0700

    Update script.js

--- static-places/script.js
+++ static-places/script.js
@@ -13,8 +13,8 @@ function staticLoadPlaces() {
             name: 'Pokèmon',
             location: {
                 // decomment the following and add coordinates:
-                // lat: <your-latitude>,
-                // lng: <your-longitude>,
+                lat: 37.377225,
+                lng:  -122.033120,
             },
         },
     ];
